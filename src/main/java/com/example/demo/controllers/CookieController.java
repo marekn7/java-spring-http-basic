531bd5279dae9c1c110052eecec00647178948b5
Marek Nenál <nenalmar@fel.cvut.cz>
2018-09-13 13:10:48 +0200

    [#7] Cookie counter

--- src/main/java/com/example/demo/controllers/CookieController.java
+++ src/main/java/com/example/demo/controllers/CookieController.java
@@ -5,18 +5,44 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLEncoder;
 
 @Controller
+@RequestMapping("/cookie")
 public class CookieController {
 
-    @RequestMapping("/cookie")
+    @RequestMapping("")
     @ResponseBody
     public String cookieIndex(HttpServletResponse request) {
 
-        Cookie cookie = new Cookie("rohlik", "cz");
-        request.addCookie(cookie);
+        String title = "<h1>Cookie</h1>";
 
-        return "Cookie";
+        String counter = "<a href='http://localhost:8080/cookie/counter'>Link - Cookie counter</a>";
+
+        Cookie cookie1 = new Cookie("rohlik1", "cz");
+        request.addCookie(cookie1);
+
+        Cookie cookie2 = new Cookie("rohlik2", "de");
+        request.addCookie(cookie2);
+
+        Cookie cookie3 = new Cookie("rohlik3", "hu");
+        request.addCookie(cookie3);
+
+        Cookie cookie4 = new Cookie("rohlik4", "po");
+        request.addCookie(cookie4);
+
+        return title + counter;
     }
+
+    @RequestMapping("/counter")
+    @ResponseBody
+    public String cookieCounter(@CookieValue(value = "counter", defaultValue = "0") Long counter, HttpServletResponse response) {
+
+        counter++;
+
+        Cookie cookieCounter = new Cookie("counter", counter.toString());
+        response.addCookie(cookieCounter);
+
+        return counter.toString();
+    }
+
 }
